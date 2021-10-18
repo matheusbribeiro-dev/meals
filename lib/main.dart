9e107d2a99caa98dd0dcca0cfc4e5c04e4a0633a
Matheus Ribeiro <matheusbrdev@gmail.com>
2021-10-18 10:47:07 -0300

    Added bottom tab bar navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
-import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
+import 'package:meals/screens/tabs_screen.dart';
 import 'package:meals/utils/app_routes.dart';
  
 void main() => runApp(MyApp());
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         canvasColor: const Color.fromRGBO(255, 254, 229, 1)
       ),
       routes: {
-        AppRoutes.home : (ctx) => const CategoriesScreen(),
+        AppRoutes.home : (ctx) => const TabsScreen(),
         AppRoutes.categoriesMeals : (ctx) => const CategoriesMealsScreen(),
         AppRoutes.mealDetail : (ctx) => const MealDetailScreen()
       }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -2,29 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:meals/components/category_item.dart';
 import 'package:meals/data/dummy_data.dart';
 
-class CategoriesScreen extends StatelessWidget
-{
+class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Vamos cozinhar?'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
-          mainAxisSpacing: 20
-        ),
-        children: dummyCategories.map((cat) {
-          return CategoryItem(cat);
-        }).toList(),
-      ),
+          mainAxisSpacing: 20),
+      children: dummyCategories.map((cat) {
+        return CategoryItem(cat);
+      }).toList(),
     );
   }
 }

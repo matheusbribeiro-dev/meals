1c9a67c97c1ef836cb5d90f287c1c2eb5d969d7b
Matheus Ribeiro <matheusbrdev@gmail.com>
2021-10-19 10:30:15 -0300

    The filters feature is completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/models/settings.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/screens/settings_screen.dart';
@@ -7,8 +10,37 @@ import 'package:meals/utils/app_routes.dart';
  
 void main() => runApp(MyApp());
  
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
+  Settings settings = Settings();
+  List<Meal> _availableMeals = dummyMeals;
+
+  void _filterMeals(Settings settings) 
+  {
+    setState(() {
+      this.settings = settings;
+
+      _availableMeals = dummyMeals.where((meal) {
+        final filterGlutten = settings.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = settings.isVegan && !meal.isVegan;
+        final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
+
+        return 
+          !filterGlutten
+          && !filterLactose
+          && !filterVegan
+          && !filterVegetarian;
+      }).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +63,9 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         AppRoutes.home : (ctx) => const TabsScreen(),
-        AppRoutes.categoriesMeals : (ctx) => const CategoriesMealsScreen(),
+        AppRoutes.categoriesMeals : (ctx) => CategoriesMealsScreen(_availableMeals),
         AppRoutes.mealDetail : (ctx) => const MealDetailScreen(),
-        AppRoutes.settings : (ctx) => const SettingsScreen()
+        AppRoutes.settings : (ctx) => SettingsScreen(settings, _filterMeals)
       }
     );
   }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -3,7 +3,13 @@ import 'package:meals/components/main_drawer.dart';
 import 'package:meals/models/settings.dart';
 
 class SettingsScreen extends StatefulWidget {
-  const SettingsScreen({ Key? key }) : super(key: key);
+  final Settings settings;
+  final Function(Settings) onSettingsChanged;
+
+  const SettingsScreen(
+    this.settings,
+    this.onSettingsChanged,
+    { Key? key }) : super(key: key);
 
   @override
   State<SettingsScreen> createState() => _SettingsScreenState();
@@ -11,7 +17,14 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
 
-  var settings = Settings();
+  var settings;
+
+  @override
+  void initState()
+  {
+    super.initState();
+    settings = widget.settings;
+  }
   
   Widget _createSwitch(
     String title,
@@ -24,7 +37,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
       title: Text(title),
       subtitle: Text(subtitle),
       value: value,
-      onChanged: onChanged
+      onChanged: (value) {
+        onChanged(value);
+        widget.onSettingsChanged(settings);
+      }
     );
   }
 
